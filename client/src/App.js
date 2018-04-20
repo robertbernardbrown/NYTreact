c5253351cba2f3cf3d59ef8f66b85c4545914a8d
Bob Brown <robertbernardbrown@gmail.com>
2018-04-20 11:14:59 -0400

    adds wrapper component

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import Footer from "./components/Footer"
 import "./App.css";
+import Wrapper from "./components/Wrapper";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Wrapper>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Footer/>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <Footer/>
-      </div>
+      </Wrapper>
     );
   }
 }
