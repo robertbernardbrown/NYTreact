0843901fd0493f299a2c64e9f214bd7ac46bd814
Bob Brown <robertbernardbrown@gmail.com>
2018-04-20 15:00:33 -0400

    creates functional search panel that allows user to search for different articles and populate the array state

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,8 +1,14 @@
-import React from "react";
 import axios from "axios";
 
-export default API = () => {
-    getArticles = () => {
-        axios.get()
+export default {
+    getArticles: (topic, start, end) => {
+        return axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json", {
+            params: {
+                'api-key'   : "3d18b69255a54a70b618943840ed2390",
+                'q'         : topic,
+                'begin_date': start,
+                'end_date'  : end
+            }
+        })
     }
 }
